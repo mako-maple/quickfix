c8ef339ec8c17dae2e1231eb0cdbe9170e6f0bdd
Oracle Public Cloud User <opc@instance-20220511-0535.subnet08080913.vcn08080913.oraclevcn.com>
2022-06-06 07:18:04 +0000

    1_TestRequest

---
+++ examples/client/1_TestRequest.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+/* MsgType = 1  */
+void Application::TestRequest()
+{
+  FIX44::TestRequest message;
+  SetMessageHeader(message);
+
+  // send message( Quote & Trade )
+  std::vector<std::string> type{"1-Quote", "2-Trade"};
+  for (std::string ID : type)
+  {
+    message.set(FIX::TestReqID(ID));
+    FIX::Session::sendToTarget(message, *ID.c_str() == '1' ? SessionTypeQUOTE : SessionTypeTRADE);
+    std::cout << "<1> TestRequest: " << ID << std::endl;
+  }
+}
